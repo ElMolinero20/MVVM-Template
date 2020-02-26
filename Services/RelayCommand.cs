fdc046216a9720dd6ea8764f8752092bd94fb26c
Marcel Müller <mueller.marcel69@gmail.com>
2020-02-26 18:20:47 +0100

    Initial commit with existing classes from library before

---
+++ Services/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM.Services
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _executeHandler;
+        private readonly Predicate<object> _canExecuteHandler;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _executeHandler = execute ?? throw new ArgumentNullException("Execute can not be null");
+            _canExecuteHandler = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteHandler == null ? true : _canExecuteHandler(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeHandler(parameter);
+        }
+    }
+}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace MVVM.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
