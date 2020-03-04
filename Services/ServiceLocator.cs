350bcf14a2deef80563553a0f4503f5ae0620043
Marcel Müller <Marcel.Mueller@homag.com>
2020-03-04 16:45:44 +0100

    Removed unnecessary using directives

--- Services/ServiceLocator.cs
+++ Services/ServiceLocator.cs
@@ -1,10 +1,6 @@
 ﻿using MVVM.Services.Locator;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM.Services
 {
