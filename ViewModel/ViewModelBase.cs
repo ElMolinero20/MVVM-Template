1b5bd3f91e52ce7374e12d52d94abd6749be417f
Marcel Müller <mueller.marcel69@gmail.com>
2020-03-25 23:34:26 +0100

    Improved the RaisePropertyChanged-Method and added comments

--- ViewModel/ViewModelBase.cs
+++ ViewModel/ViewModelBase.cs
@@ -6,15 +6,20 @@ namespace MVVM.ViewModel
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Implementation of the INotifyPropertyChanged interface.
+        /// It provides the PropertyChanged event which is fired when user controls are activated.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
-        private void RaisePropertyChanged(string property)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        }
+
+        /// <summary>
+        /// Fires the PropertyChanged.
+        /// </summary>
+        /// <param name="expression">Takes the lambda expression without parameters and returns the property.</param>
         public void RaisePropertyChanged(Expression<Func<object>> expression)
         {
             string propertyName = ((MemberExpression)expression.Body).Member.Name;
-            RaisePropertyChanged(propertyName);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
